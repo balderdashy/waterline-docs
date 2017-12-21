009fad24affb63c3821ab898cbd112ac2037697b
Mike McNeil <mikermcneil@users.noreply.github.com>
2017-12-20 18:45:36 -0600

    Update delete-password-json.js

--- examples/src/delete-password-json.js
+++ examples/src/delete-password-json.js
@@ -1,3 +1,32 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+//  ██╗    ██╗ █████╗ ██████╗ ███╗   ██╗██╗███╗   ██╗ ██████╗ ██╗
+//  ██║    ██║██╔══██╗██╔══██╗████╗  ██║██║████╗  ██║██╔════╝ ██║
+//  ██║ █╗ ██║███████║██████╔╝██╔██╗ ██║██║██╔██╗ ██║██║  ███╗██║
+//  ██║███╗██║██╔══██║██╔══██╗██║╚██╗██║██║██║╚██╗██║██║   ██║╚═╝
+//  ╚███╔███╔╝██║  ██║██║  ██║██║ ╚████║██║██║ ╚████║╚██████╔╝██╗
+//   ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝
+//                                                               
+// WARNING: The following code may be out of date!
+//
+// > The official Waterline documentation now lives in the Sails documentation:
+// >   http://sailsjs.com/docs/concepts/models-and-orm/).
+// 
+// This content has only been left here to try and maintain the quality of older search engine links.
+// Please be sure and visit the link above for the most recent and up-to-date docs.
+//  [?] If you're unsure, drop by https://sailsjs.com/support
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+
+
+
+
+
+
+
+
+
+
+
 /**
  * A waterline method showing the use of `toJSON` to remove the password.
  */
